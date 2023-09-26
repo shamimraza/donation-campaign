4f34265bae193d7b5c821ccb94cf5444110d7e2f
Shamim raza <shamimraza411@gmail.com>
2023-09-27 03:45:53 +0600

    error page

--- src/errorElements/ErrorElements.jsx
+++ src/errorElements/ErrorElements.jsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 const ErrorElements = () => {
   return (
-    <div>
-      <h2>Opps !!!!!!!!!!!!</h2>
-      <Link to="/">Go Back Home</Link>
+    <div className="text-center mt-56">
+      <h2 className="font-bold mt-4 text-7xl">
+        <span className="text-red-400">404 </span>
+      </h2>
+      <h2 className="font-bold text-5xl mt-5">Page Not Found</h2>
+      <Link to="/">
+        <button className="btn btn-error mt-4 text-white">Go Back Home</button>
+      </Link>
     </div>
   );
 };
